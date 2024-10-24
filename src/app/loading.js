bf58d283cfef94205e2cd7ef4a9a41cec2da197c
taissir alem <taissir.alem@gmail.com>
2024-10-24 12:35:13 +0100

    24

---
+++ src/app/loading.js
@@ -0,0 +1,14 @@
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    ></div>
+  );
+};
+
+export default Loading;
